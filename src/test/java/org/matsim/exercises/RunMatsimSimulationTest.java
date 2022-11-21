12ece885c0f247935dd3348d015ab0afd577a77d
Chengqi Lu <43133404+luchengqi7@users.noreply.github.com>
2022-11-21 15:20:47 +0100

    update

--- src/test/java/org/matsim/exercises/RunMatsimSimulationTest.java
+++ src/test/java/org/matsim/exercises/RunMatsimSimulationTest.java
@@ -1,18 +1,39 @@
 package org.matsim.exercises;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.testcases.MatsimTestUtils;
 
 import static org.junit.Assert.assertEquals;
 
 public class RunMatsimSimulationTest {
+    @Rule
+    public MatsimTestUtils utils = new MatsimTestUtils();
 
     @Test
     public void test() {
-        int x = 1;
-        int y = 2;
+        System.out.println("input directory: " + utils.getClassInputDirectory());
+        System.out.println("output directory: " + utils.getOutputDirectory());
+
+        Config config = ConfigUtils.createConfig();
+        config.network().setInputFile(utils.getClassInputDirectory() + "/network.xml.gz");
+        config.plans().setInputFile(utils.getClassInputDirectory() + "/plans.xml.gz");
+        config.controler().setOutputDirectory(utils.getOutputDirectory());
+        config.controler().setLastIteration(1);
+
+        Scenario scenario = ScenarioUtils.loadScenario(config);
+        Controler controler = new Controler(scenario);
+        controler.run();
+
+        System.out.println("output directory: " + utils.getOutputDirectory());
+        System.out.println("input directory: " + utils.getInputDirectory());
 
         System.out.println("test is complete");
-        assertEquals(3, y + x);
     }
 
 }
